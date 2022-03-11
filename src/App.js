715ad8640a127a4f334a2805955489e68c96181b
HamishLeFol <arthur.mestries@gmail.com>
2022-03-11 09:46:54 +0100

    fix mise en forme et <Route>
    Mise en page de index.html.
    Mise en place de <Route> dans App.js et léger changement dans l'ordre des Route

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import Accueil from './Accueil.js'
+import Connexion from './Connexion.js'
+//import Deconnexion from './Deconnexion.js'
 
 import EnregistrerCours from './EnregistrerCours.js'
 import ListerCours from './ListerCours.js'
 import RechercheCours from './RechercheCours.js'
-import DeleteCours from './DeleteCours.js'
-import UpdateCours from './UpdateCours.js'
-
-import EnregistrerComposante from './EnregistrerComposante.js'
-import ListerComposante from './ListerComposante.js'
+import SupprimerCours from './SupprimerCours.js'
+import ModifierCours from './ModifierCours.js'
 
 import EnregistrerIntervenant from './EnregistrerIntervenant.js'
 import ListerIntervenant from './ListerIntervenant.js'
@@ -24,31 +23,31 @@ import RechercheGestionnaire from './RechercheGestionnaire.js'
 import ModifierGestionnaire from './ModifierGestionnaire.js'
 import SupprimerGestionnaire from './SupprimerGestionnaire.js'
 
-import EnregistrerFiliere_langue from './EnregistrerFiliere_langue.js'
-import ListerFiliere_langue from './ListerFiliere_langue.js'
-import RechercheFiliere_langue from './RechercheFiliere_langue.js'
-import ModifierFiliere_langue from './ModifierFiliere_langue.js'
-import SupprimerFiliere_langue from './SupprimerFiliere_langue.js'
+import EnregistrerComposante from './EnregistrerComposante.js'
+import ListerComposante from './ListerComposante.js'
 
-import Connexion from './Connexion.js'
+
 
 class App extends React.Component {
 
   constructor(props) {
     super(props)
   }
-
+  //<Route exact path="/Deconnexion" element={<Deconnxion/>}/>
   render() {
     return (
       <div>
         <Router>
           <Routes>
             <Route exact path="/" element={<Accueil />} />
+            <Route exact path="/Connexion" element={<Connexion/>}/>
+            
+
             <Route exact path="/EnregistrerCours" element={<EnregistrerCours />} />
             <Route exact path="/ListerCours" element={<ListerCours />} />
             <Route exact path="/RechercheCours" element={<RechercheCours />}/>
-            <Route exact path="/DeleteCours" element={<DeleteCours />}/>
-            <Route exact path="UpdateCours" element={<UpdateCours/>}/>
+            <Route exact path="/SupprimerCours" element={<SupprimerCours />}/>
+            <Route exact path="ModifierCours" element={<ModifierCours/>}/>
 
             <Route exact path="/EnregistrerIntervenant" element={<EnregistrerIntervenant/>} />
             <Route exact path="/ListerIntervenant" element={<ListerIntervenant/>} />
@@ -62,16 +61,10 @@ class App extends React.Component {
             <Route exact path="/ModifierGestionnaire" element={<ModifierGestionnaire/>}/>
             <Route exact path="/SupprimerGestionnaire" element={<SupprimerGestionnaire/>}/>
 
-            <Route exact path="/EnregistrerFiliere_langue" element={<EnregistrerFiliere_langue/>} />
-            <Route exact path="/ListerFiliere_langue" element={<ListerFiliere_langue/>} />
-            <Route exact path="/RechercheFiliere_langue" element={<RechercheFiliere_langue/>} />
-            <Route exact path="/ModifierFiliere_langue" element={<ModifierFiliere_langue/>}/>
-            <Route exact path="/SupprimerFiliere_langue" element={<SupprimerFiliere_langue/>}/>
-
             <Route exact path="/EnregistrerComposante" element={<EnregistrerComposante />} />
             <Route exact path="/ListerComposante" element={<ListerComposante />} />
 
-            <Route exact path="/Connexion" element={<Connexion/>}/>
+            
 
           </Routes>
         </Router>
