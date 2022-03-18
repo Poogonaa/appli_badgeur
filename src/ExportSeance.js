00e4b2ad3c24d64ab69ec3555229c9d978e47f3c
HamishLeFol <arthur.mestries@gmail.com>
2022-03-18 09:44:35 +0100

    Fix ExportSeance.js
    Correction sur l'Export des données des Séances. Fonctionne correctement.

--- src/ExportSeance.js
+++ src/ExportSeance.js
@@ -1,28 +1,22 @@
-/*
-import React from 'react';
-import axios from 'axios'
-import { DataGrid, GridToolbarExport,
-GridToolbarContainer } from '@material-ui/data-grid';
-  
-const columns = [
-  { field: 'id', headerName: 'ID', width: 100 },
-  { field: 'duree', headerName: 'Durée (min)', width: 200 },
-  { field: 'valide', headerName: 'Validé ?', width: 150 },
-  { field: 'commentaire', headerName: 'Commentaire', width: 200 },
-
 import React, { Component } from 'react';
 import { CSVLink } from "react-csv";
 
 const headers = [
-  { label: "Id Utilisateur", key: "uti_id"},
-  { label: "Id Séance", key: "sea_id" },
-  { label: "Durée (min)", key: "dureeEffective" },
-  { label: "Validé ?", key: "estEffectue" },
+  { label: "Login Intervenant", key: "intervenantDto.login"},
+  { label: "Prénom", key: "intervenantDto.prenom"},
+  { lable: "Nom", key: "intervenantDto.nom"},
+
+  { label: "Intitulé Cours", key: "creneauDto.coursDto.intitule"},
+  { label: "Date Séance", key: "creneauDto.date" },
+  { label: "Heure Séance", key: "creneauDto.heure_debut" },
+  { label: "Durée Prévue (min)", key: "creneauDto.duree" },
+  { label: "Salle", key: "creneauDto.salle" },
+
+  { label: "Durée Effective (min)", key: "dureeEffective" },
   { label: "Commentaire", key: "commentaire" }
-  
 ];
  
-class AsyncCSV extends Component {
+class ExportSeance extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -31,6 +25,7 @@ class AsyncCSV extends Component {
     this.csvLinkEl = React.createRef();
   }
  
+  //on récupère une liste des Séances effectuées et validé par tous les Intervenants
   getUserList = () => {
     /*let retourFetch = fetch('/seancesformations/valide/intervenant')
                 .then(res => res.json());
@@ -39,9 +34,11 @@ class AsyncCSV extends Component {
     .then(res => res.json());
   }
  
+  /*
+    on récupère les données
+  */
   downloadReport = async () => {
     const data = await this.getUserList();
-    console.log(data);
     this.setState({ data: data }, () => {
       setTimeout(() => {
         this.csvLinkEl.current.link.click();
